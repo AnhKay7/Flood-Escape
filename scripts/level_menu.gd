808faea717bc8e719803e49360c9e7c5fb7b1d83
thinh2929 <thinhvippro2929@gmail.com>
2026-05-20 16:53:35 +0700

    hoàn thiện cơ bản menu

--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -1,10 +1,17 @@
 extends Control
 
 @onready var back_button = $Control/BackBtn
+@onready var lv1_button = $Control/MarginContainer/VBoxContainer/VScrollBar/GridContainer/Level1Btn
+@onready var lv2_button = $Control/MarginContainer/VBoxContainer/VScrollBar/GridContainer/Level2Btn
 
 func _ready():
 	# Kết nối tín hiệu (Signal) từ nút Back
 	back_button.pressed.connect(_on_back_pressed)
+	lv1_button.pressed.connect(_on_lv1_pressed)
+	
+func _on_lv1_pressed():
+	# Chuyển về màn hình chính
+	get_tree().change_scene_to_file("res://scenes/Level/Level1/Level1.tscn")
 
 func _on_back_pressed():
 	# Chuyển về màn hình chính
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,21 +2,23 @@ extends Control
 
 @onready var play_button = $Control/MarginContainer/VBoxContainer/Buttons/PlayButton
 @onready var exit_button = $Control/MarginContainer/VBoxContainer/Buttons/ExitButton
+@onready var setting_button = $Control/MarginContainer/VBoxContainer/Buttons/SettingsButton
 
 func _ready():
 	# Kết nối tín hiệu (Signal) từ các nút
 	play_button.pressed.connect(_on_play_pressed)
 	exit_button.pressed.connect(_on_exit_pressed)
+	setting_button.pressed.connect(_on_setting_pressed)
 
 func _on_play_pressed():
 	# In ra console khi bắt đầu game
 	print("Starting Game...")
 
-	# Đợi 0.5 giây trước khi chuyển cảnh
-	await get_tree().create_timer(0.5).timeout
-
 	# Chuyển sang level đầu tiên
 	get_tree().change_scene_to_file("res://scenes/menu/LevelsMenu.tscn")
+
+func _on_setting_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu/Settings.tscn")
 
 func _on_exit_pressed():
 	# Đóng ứng dụng
