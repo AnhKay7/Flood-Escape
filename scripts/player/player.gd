270f1c68300b15c330066aa0399b1a7c7c8088b8
AnhKay7 <anhsk69@gmail.com>
2026-05-23 18:20:23 +0700

    add water

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,3 +3,11 @@ class_name Player extends BaseCharacter
 func _ready() -> void:
 	
 	pass
+
+
+func _on_water_trigger_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_acid_water_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
